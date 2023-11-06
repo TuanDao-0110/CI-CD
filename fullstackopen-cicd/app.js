46805430b23468146461fa9dc9c6387533ff43c6
TuanDao-0110 <rickytuan1991@gmail.com>
2023-11-06 22:02:03 +0200

    add health check

--- fullstackopen-cicd/app.js
+++ fullstackopen-cicd/app.js
@@ -8,7 +8,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
+  res.send('2') 
+})
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 app.listen(PORT, () => {
   console.log('server started on port 5000')
--- fullstackopen-cicd/build_step.sh
+++ fullstackopen-cicd/build_step.sh
@@ -5,7 +5,8 @@ echo "Build script"
 # add the commands here
 
 npm install
+echo '👉installed packages successfully'
 npm run build
+echo '🌟 build completed successfully'
+echo '😀starting server'
 npm run start-prod
-
-echo "Finished"
