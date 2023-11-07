d676eb1c266aa9e6cb133686d5791ce1ffd39625
TuanDao-0110 <rickytuan1991@gmail.com>
2023-11-07 14:32:34 +0200

    updat

--- fullstackopen-cicd/app.js
+++ fullstackopen-cicd/app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 app.get('/version', (req, res) => {
-  res.send('150') 
+  res.send('200') 
 })
 app.get('/health', (req, res) => {
   res.send('ok')
