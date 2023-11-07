348f189bae4e2f87dba04acb75063c1c93e64df1
TuanDao-0110 <rickytuan1991@gmail.com>
2023-11-07 14:21:47 +0200

    fixed pipline

--- fullstackopen-cicd/app.js
+++ fullstackopen-cicd/app.js
@@ -1,4 +1,4 @@
-
+/* eslint-disable */
 
 const express = require('express')
 const app = express()
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 app.get('/version', (req, res) => {
-  res.send('3') 
+  res.send('2') 
 })
 app.get('/health', (req, res) => {
   res.send('ok')
